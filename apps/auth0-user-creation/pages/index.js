ba35375b493ae3bbe2f62020622cd6c27cb68e39
Aswin V <vaswin91@gmail.com>
2022-04-20 21:43:32 +0530

    Auth0 user creation example

---
+++ apps/auth0-user-creation/pages/index.js
@@ -0,0 +1,17 @@
+import dynamic from 'next/dynamic';
+
+const OAuthComponentWithNoSSR = dynamic(() => import('components/oauth.jsx'), {
+  ssr: false,
+});
+
+export default function IndexPage() {
+  return (
+    <div>
+      <OAuthComponentWithNoSSR />
+      <br />
+      <br />
+      This is a demo app to showcase BoxyHQ&apos;s SAML integration. It is implemented as an OAuth 2.0 flow
+      and can be used with most standard OAuth 2.0 libraries out there.
+    </div>
+  );
+}
