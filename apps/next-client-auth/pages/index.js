8d097fc755c531ba0c00d161d86011f5790ddba3
Deepak Prabhakara <deepak@boxyhq.com>
2022-05-02 21:03:17 +0100

    added jsconfig file to next-client-auth

--- apps/next-client-auth/pages/index.js
+++ apps/next-client-auth/pages/index.js
@@ -10,9 +10,8 @@ export default function IndexPage() {
       <OAuthComponentWithNoSSR />
       <br />
       <br />
-      This is a demo app to showcase BoxyHQ's SAML integration. It is
-      implemented as an OAuth 2.0 flow and can be used with most standard OAuth
-      2.0 libraries out there.
+      This is a demo app to showcase BoxyHQ&apos;s SAML integration. It is implemented as an OAuth 2.0 flow
+      and can be used with most standard OAuth 2.0 libraries out there.
     </div>
   );
 }
