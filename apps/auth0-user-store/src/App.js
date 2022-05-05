0fddbd7bf7a4a94e0c49c45561c19156fefe0028
Aswin V <vaswin91@gmail.com>
2022-05-05 22:14:53 +0530

    Redirect to /tenant after checking authenticated state

--- apps/auth0-user-store/src/App.js
+++ apps/auth0-user-store/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
@@ -7,7 +7,6 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './views/Home';
 import Profile from './views/Profile';
-import ExternalApi from './views/ExternalApi';
 import { useAuth0 } from '@auth0/auth0-react';
 import history from './utils/history';
 
@@ -16,10 +15,24 @@ import './App.css';
 
 // fontawesome
 import initFontAwesome from './utils/initFontAwesome';
+import TenantForm from './views/TenantForm';
 initFontAwesome();
 
+const TENANT_RE = /[?&]tenant=[^&]+/;
+
 const App = () => {
-  const { isLoading, error } = useAuth0();
+  const { isLoading, error, isAuthenticated } = useAuth0();
+
+  const searchParams = window.location.search;
+
+  const hasTenantParams = TENANT_RE.test(searchParams);
+  const tenant = hasTenantParams && new URLSearchParams(searchParams).get('tenant');
+
+  useEffect(() => {
+    if (!tenant && !isLoading && !isAuthenticated) {
+      history.push(`/tenant`);
+    }
+  }, [tenant, isAuthenticated, isLoading]);
 
   if (error) {
     return <div>Oops... {error.message}</div>;
@@ -37,7 +50,7 @@ const App = () => {
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/profile' component={Profile} />
-            <Route path='/external-api' component={ExternalApi} />
+            <Route path='/tenant' component={TenantForm} />
           </Switch>
         </Container>
         <Footer />
