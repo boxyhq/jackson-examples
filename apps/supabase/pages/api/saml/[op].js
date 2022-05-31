f275fba0bba638bd4e474abb0609105537abc2cb
Aswin V <vaswin91@gmail.com>
2022-06-01 00:27:28 +0700

    WIP

--- apps/supabase/pages/api/saml/[op].js
+++ apps/supabase/pages/api/saml/[op].js
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
+import axios from 'axios';
+export default async function handler(req, res) {
   const { op } = req.query;
-  switch (op) {
-    case 'add':
-    // POST /admin/saml/config/add
-    case 'list':
-    // GET /admin/saml/config
+  try {
+    switch (op) {
+      case 'add':
+      // POST /admin/saml/config/add
+      case 'list':
+        // GET /admin/saml/config
+        const _response = await axios.get(`${process.env.API_URL}/admin/saml/config`, {
+          headers: { Authorization: `Bearer ${process.env.JACKSON_API_KEY}` },
+        });
+
+      case 'update':
+      // PATCH /admin/saml/config/update
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
-  // check if admin user
-  res.status(200).json({ name: 'John Doe' });
 }
