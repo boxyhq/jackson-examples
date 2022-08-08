eed49931f2bbf9bedd52c2d6f4c40a11cf4783b5
Aswin V <vaswin91@gmail.com>
2022-08-08 17:47:52 +0530

    Add redirect_uri for code exchange

--- apps/express/routes/index.js
+++ apps/express/routes/index.js
@@ -138,6 +138,7 @@ router.get('/sso/callback', async (req, res, next) => {
     code,
     client_id: `tenant=${tenant}&product=${product}`,
     client_secret: 'dummy',
+    redirect_uri: defaultRedirectUrl,
   };
 
   try {
