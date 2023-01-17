23b149f897c8278ff04ff7cd7fb7959127f4ded0
Kiran <kiran@boxyhq.com>
2023-01-17 11:40:39 +0530

    update the example app

--- apps/directory-sync-embedded/pages/config.tsx
+++ apps/directory-sync-embedded/pages/config.tsx
@@ -1,38 +1,38 @@
-import type { GetServerSideProps } from "next";
-import type { Directory } from "@boxyhq/saml-jackson";
-import Container from "../components/Container";
-import config from "../lib/constants";
-import { findOrCreateDirectory } from "../lib/directory";
+import type { GetServerSideProps } from 'next';
+import type { Directory } from '@boxyhq/saml-jackson';
+import Container from '../components/Container';
+import config from '../lib/constants';
+import { findOrCreateDirectory } from '../lib/directory';
 
 export default function Config({ directory }: { directory: Directory }) {
   return (
-    <Container title="Create Directory Sync Connections">
-      <div className="space-y-4">
-        <h2 className="mb-5 text-2xl">Configure Directory Sync</h2>
-        <table className="w-full text-sm text-gray-800">
+    <Container title='Create Directory Sync Connections'>
+      <div className='space-y-4'>
+        <h2 className='mb-5 text-2xl'>Configure Directory Sync</h2>
+        <table className='w-full text-sm text-gray-800'>
           <tbody>
             <tr>
-              <td className="uppercase">Directory ID</td>
+              <td className='uppercase'>Directory ID</td>
               <td>{directory.id}</td>
             </tr>
             <tr>
-              <td className="uppercase py-4">Name</td>
+              <td className='py-4 uppercase'>Name</td>
               <td>{directory.name}</td>
             </tr>
             <tr>
-              <td className="uppercase">Tenant</td>
+              <td className='uppercase'>Tenant</td>
               <td>{directory.tenant}</td>
             </tr>
             <tr>
-              <td className="uppercase py-4">Product</td>
+              <td className='py-4 uppercase'>Product</td>
               <td>{directory.product}</td>
             </tr>
             <tr>
-              <td className="uppercase">SCIM Endpoint</td>
+              <td className='uppercase'>SCIM Endpoint</td>
               <td>{directory.scim.endpoint}</td>
             </tr>
             <tr>
-              <td className="uppercase py-4">SCIM Secret</td>
+              <td className='py-4 uppercase'>SCIM Secret</td>
               <td>{directory.scim.secret}</td>
             </tr>
           </tbody>
--- apps/directory-sync-embedded/pages/groups.tsx
+++ apps/directory-sync-embedded/pages/groups.tsx
@@ -1,22 +1,22 @@
-import type { GetServerSideProps } from "next";
-import Container from "../components/Container";
-import { Group } from "../types";
-import jackson from "../lib/jackson";
-import config from "../lib/constants";
+import type { GetServerSideProps } from 'next';
+import Container from '../components/Container';
+import { Group } from '../types';
+import jackson from '../lib/jackson';
+import config from '../lib/constants';
 
 export default function Groups({ groups }: { groups: Group[] }) {
   return (
-    <Container title="Groups">
-      <div className="space-y-4">
-        <h2 className="mb-5 text-2xl">Groups ({groups.length})</h2>
+    <Container title='Groups'>
+      <div className='space-y-4'>
+        <h2 className='mb-5 text-2xl'>Groups ({groups.length})</h2>
       </div>
-      <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-        <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+      <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
+        <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
           <tr>
-            <th scope="col" className="px-6 py-3">
+            <th scope='col' className='px-6 py-3'>
               Name
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope='col' className='px-6 py-3'>
               Raw
             </th>
           </tr>
@@ -26,12 +26,9 @@ export default function Groups({ groups }: { groups: Group[] }) {
             return (
               <tr
                 key={group.id}
-                className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
-              >
-                <td className="px-6 py-4">{group.name}</td>
-                <td className="px-6 py-4">
-                  {group.raw ? JSON.stringify(group.raw) : ""}
-                </td>
+                className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'>
+                <td className='px-6 py-4'>{group.name}</td>
+                <td className='px-6 py-4'>{group.raw ? JSON.stringify(group.raw) : ''}</td>
               </tr>
             );
           })}
@@ -47,9 +44,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   const tenant = config.tenant;
   const product = config.product;
 
-  const { data: groups } = await directorySync.groups
-    .setTenantAndProduct(tenant, product)
-    .list({});
+  const { data: groups } = await directorySync.groups.setTenantAndProduct(tenant, product).list({});
 
   return {
     props: {
