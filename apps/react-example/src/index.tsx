765482dbd6a3afd2ed2aea09a951c843bcd2f1c6
Kiran K <mailtokirankk@gmail.com>
2024-01-22 13:42:28 +0530

    fix

--- apps/react-example/src/index.tsx
+++ apps/react-example/src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
